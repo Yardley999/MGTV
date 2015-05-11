7204e92ff79fa675ed932d6aaaeefd3d89151b5c
Rocky Lai <rockylai@qq.com>
2015-05-11 18:34:40 +0800

    add controls on main page

---
+++ src/MGTV/MGTV/Controls/Category.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MGTV.Controls
+{
+    public sealed partial class Category : UserControl
+    {
+
+        #region Dependency Property
+
+        public static readonly DependencyProperty CategoryNameProperty = DependencyProperty.Register("CategoryName", typeof(string), typeof(Category), new PropertyMetadata(string.Empty));
+
+        #endregion
+
+        #region CLR Wrapper
+
+        public string CategoryName
+        {
+            get { return (string)GetValue(CategoryNameProperty); }
+            set { SetValue(CategoryNameProperty, value); }
+        }
+
+        #endregion
+
+        public Category()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ src/MGTV/MGTV/Controls/ItemTile.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MGTV.Controls
+{
+    public sealed partial class ItemTile : UserControl
+    {
+
+        #region Dependency Property
+
+        public static readonly DependencyProperty IntroProperty = DependencyProperty.Register("Intro", typeof(string), typeof(ItemTile), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(ItemTile), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(string), typeof(ItemTile), new PropertyMetadata(string.Empty));
+
+        #endregion
+
+        #region CLR Wrapper
+
+        public string Image
+        {
+            get { return (string)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public string Intro
+        {
+            get { return (string)GetValue(IntroProperty); }
+            set { SetValue(IntroProperty, value); }
+        }
+
+        #endregion
+
+
+        public ItemTile()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+
+            Test();
+        }
+
+        private void Test()
+        {
+            this.Image = "ms-appx:///Assets/test.jpg";
+            this.Intro = "爸爸去哪里了啊unalilaj阿斯顿法定";
+            this.Title = "爸爸去哪儿了";
+        }
+    }
+}

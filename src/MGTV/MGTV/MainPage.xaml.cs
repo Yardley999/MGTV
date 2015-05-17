56354c82a7213d1e8f1626f3f8bc70993ca923d6
Rocky Lai <rockylai@qq.com>
2015-05-17 12:24:29 +0800

    change background

--- src/MGTV/MGTV/MainPage.xaml.cs
+++ src/MGTV/MGTV/MainPage.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using MGTV.MG.API;
 using MGTV.ViewModels;
+using SharedFx.UI.Animations;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace MGTV
 {
@@ -16,6 +22,7 @@ namespace MGTV
             this.InitializeComponent();
             this.root.DataContext = viewModel;
             NavigationCacheMode = NavigationCacheMode.Required;
+            Init();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -26,9 +33,14 @@ namespace MGTV
             {
                 return;
             }
-
-            LoadDataAysnc();
+            else
+            {
+                LoadDataAysnc();
+            }
         }
+
+
+        #region Data
 
         public async Task LoadDataAysnc()
         {
@@ -102,5 +114,92 @@ namespace MGTV
 
             return video;
         }
+
+        #endregion
+
+        private void ChangeBackground_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            ChangeBackground();
+        }
+
+        #region Change Background
+
+        private string currentBackground = string.Empty;
+
+        private List<string> backgourndImages = new List<string>()
+        {
+            "ms-appx:///Assets/Background/night.png",
+            "ms-appx:///Assets/Background/ice.jpg",
+            "ms-appx:///Assets/Background/ice2.jpg",
+            "ms-appx:///Assets/Background/forest.jpg"
+        };
+
+        bool isBackgroundInChanging = false;
+
+        private void Init()
+        {
+            currentBackground = RandomSelectBackground();
+            background1.Opacity = 1;
+            background1.Visibility = Visibility.Visible;
+            background1.Background = new ImageBrush() {
+                ImageSource = new BitmapImage(new Uri(currentBackground)),
+                AlignmentX = AlignmentX.Center,
+                AlignmentY = AlignmentY.Center,
+                Stretch = Stretch.UniformToFill
+            };
+
+            background2.Opacity = 0;
+            background2.Visibility = Visibility.Collapsed;
+        }
+
+        private string RandomSelectBackground()
+        {
+            var list = backgourndImages.Where(img => !img.Equals(currentBackground, StringComparison.OrdinalIgnoreCase)).ToList();
+            return list[(new Random()).Next(0, list.Count())];
+        }
+
+
+        public void ChangeBackground()
+        {
+            if (isBackgroundInChanging)
+            {
+                return;
+            }
+
+            Grid gridToShow;
+            Grid gridToHide;
+
+            if(background1.Visibility == Visibility.Collapsed)
+            {
+                gridToShow = background1;
+                gridToHide = background2;
+            }
+            else
+            {
+                gridToShow = background2;
+                gridToHide = background1;
+            }
+
+            isBackgroundInChanging = true;
+            TimeSpan duration = TimeSpan.FromSeconds(1.8);
+            gridToShow.Visibility = Visibility.Visible;
+            string imageToShow = RandomSelectBackground();
+            gridToShow.Background = new ImageBrush() {
+                ImageSource = new BitmapImage(new Uri(imageToShow, UriKind.RelativeOrAbsolute)),
+                AlignmentX = AlignmentX.Center,
+                AlignmentY = AlignmentY.Center,
+                Stretch = Stretch.UniformToFill
+            };
+
+            FadeAnimation.Fade(gridToHide, 1, 0, duration, null);
+            FadeAnimation.Fade(gridToShow, 0, 1, duration, fe =>
+            {
+                gridToHide.Visibility = Visibility.Collapsed;
+                isBackgroundInChanging = false;
+                currentBackground = imageToShow;
+            });
+        }
+        
+        #endregion
     }
 }
---
+++ src/MGTV/MGTV/SharedFx.10/UI/UIExtension.cs
@@ -0,0 +1,73 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+public static class UIExtension
+{
+    public static T GetParent<T>(this FrameworkElement element) where T : FrameworkElement
+    {
+        if (element == null)
+        {
+            return null;
+        }
+
+        var parent = VisualTreeHelper.GetParent(element);
+
+        if (parent == null)
+        {
+            return null;
+        }
+
+        while (parent != null)
+        {
+            if (parent is T)
+            {
+                return parent as T;
+            }
+            else
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+        }
+
+        return null;
+    }
+
+    public static T GetChildInVisualTree<T>(this FrameworkElement element) where T : FrameworkElement
+    {
+        if(element == null)
+        {
+            return null;
+        }
+
+        int count = VisualTreeHelper.GetChildrenCount(element);
+        if(count > 0)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i);
+                if (child is T)
+                {
+                    return child as T;
+                }
+                else
+                {
+                    if(child != null)
+                    {
+
+                        var childFrameworkElement = child as FrameworkElement;
+                        if (childFrameworkElement != null)
+                        {
+                            var childInChildFE = childFrameworkElement.GetChildInVisualTree<T>();
+                            if(childInChildFE != null)
+                            {
+                                return childInChildFE;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
